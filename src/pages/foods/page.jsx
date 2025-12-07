924f98bcc942a9c607d360f09cde246b1999b0fa
Lemuel Figueira <lfigueiradev@gmail.com>
2025-12-07 20:46:21 -0300

    feat: create Title component for FoodsPage 🎉📦✨

--- src/pages/foods/page.jsx
+++ src/pages/foods/page.jsx
@@ -1,13 +1,10 @@
+import { Title } from "./components/title";
+
+
 export const FoodsPage = () => {
   return (
     <div className="flex flex-col items-start justify-start w-full">
-      <p className="w-full flex flex-col items-start justify-start gap-1">
-        <span>Bem vindo(a) ao</span>
-        <span>
-          <strong className="font-bold text-lg uppercase">Waiter</strong>
-          <span className="pl-0.5 font-normal uppercase font-medium">App</span>
-        </span>
-      </p>
+      <Title />
     </div>
   );
 };
