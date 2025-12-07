6af3aaef2e3a061d4f7937da073dcd8433dadc4b
Marina Figueira <imarinaafigueira@gmail.com>
2025-12-07 19:32:11 -0300

    wip

--- src/pages/foods-page/FoodsPage.jsx
+++ src/pages/foods-page/FoodsPage.jsx
@@ -1,25 +1,67 @@
+import { Button } from "@/components/ui/button";
 import { FootCategories } from "./components/food-categories/FootCategories"
+import FoodItem from "./components/food-item/FoodItem"
 
-const foodList = [
-    { id: '1', name: 'Burgers', url: '' },
-    { id: '2', name: 'Pizzas', url: '' },
-    { id: '3', name: 'Drinks', url: '' }
-]
+const listItems = [
+    {
+        img: "../../../../../src/assets/imgPizzaQuatroQ.png",
+        title: "Quatro Queijos",
+        description: "Pizza de Quatro Queijos com borda tradicional",
+        price: "40,00"
+    },
+    { img: "../../../../../src/assets/imgPizzaFrango.png",
+     title: "Frango com Catupiry", 
+     description: "Pizza de Frango com Catupiry e borda tradicional", 
+     price: "40,00" },
+    { 
+    img: "../../../../../src/assets/imgPizzaMarguerita.png",
+     title: "Marguerita", 
+     description: "Pizza de Marguerita no melhor estilo italiano", 
+     price: "50,00" },
+];
 
 export const FootPage = () => {
     return (
         <div>
+            <div className="flex flex-col items-start justify-center mb-6">
+            <span className="text-gray-600">Bem-vindo(a) ao</span>
+            <span className="text-3xl font-mono text-gray-600"><strong className="text-black">WAITER</strong>APP</span>
+            </div>
 
-            <FootCategories items={
-                foodList
+            <FootCategories items={ 
+                listItems
             } />
 
-            <ul id="food-list">
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
-                <FoodItem />
+            <ul id="food-list " className="mb-24">
+                {listItems.map((item,index)=>{
+                    return (
+                   <div>
+                    <FoodItem 
+                    key={item.id}
+                    id={index}
+                    img={item.img}
+                    title={item.title}
+                    description={item.description}
+                    price={item.price}
+                    />
+                       
+ {
+ index < listItems.length - 1 && (
+                            <div className="border-b-2 border-gray-200 w-full mb-8 mt-8">
+
+                            </div>
+                        )}
+                    </div>
+                    
+                    )
+                })}
+                
             </ul>
+            <Button className="w-full h-12  rounded-4xl">
+                Novo Pedido
+            </Button>
         </div>
     )
 }
+
+export default FootPage
--- src/pages/foods-page/components/food-categories/FootCategories.jsx
+++ src/pages/foods-page/components/food-categories/FootCategories.jsx
@@ -1,13 +1,28 @@
+const listCategory=[
+    {name:"Pizzas",
+     icone: "../../../../../public/pizza.png"},
+    {name:"Bebidas",
+     icone:"../../../../../public/bebidas.png"
+    },
+    {name:"Lanches",
+     icone:"../../../../../public/lanches.png"
+    },{name:"Promoções",
+     icone:"../../../../../public/promocoes.png"
+    }]
 
 export const FootCategoryItem = ({ name, imgUrl }) => {
-    return <li>Foot Category Item</li>;
+
+    return <li className="flex   flex-col  justify-center items-center w-15 text-center">
+        <img src={imgUrl} alt="" className="w-15 h-15 p-5 bg-white rounded-4xl shadow-sm "/>
+        <span className="text-black font-bold mt-3">{name}</span>
+    </li>;
 }
 
-export const FootCategories = ({ items = [] }) => {
+export const FootCategories = () => {
     return (
-        <ul>
-            {items.map(item => (
-                <FootCategoryItem key={item.id} name={item?.name} imgUrl={item?.url} />
+        <ul className="flex flex-row gap-3 justify-between mb-6">
+            {listCategory.map(item => (
+                <FootCategoryItem key={item.id} name={item?.name} imgUrl={item?.icone} />
             ))}
         </ul>
     )
