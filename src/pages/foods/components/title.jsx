7836f2c8bc11143fbbc3bd24d52b1c52f19c56e8
Lemuel Figueira <lfigueiradev@gmail.com>
2025-12-07 23:18:17 -0300

    style: update Title component text styles and formatting ✨🆕

--- src/pages/foods/components/title.jsx
+++ src/pages/foods/components/title.jsx
@@ -1,10 +1,10 @@
 export const Title = () => {
   return (
     <p className="w-full flex flex-col items-start justify-start gap-1">
-      <span>Bem vindo(a) ao</span>
-      <span>
-        <strong className="font-bold text-lg uppercase font-title">Waiter</strong>
-        <span className="pl-0.5 font-normal uppercase font-medium">App</span>
+      <span className="text-sm font-normal leading-1">Bem vindo(a) ao</span>
+      <span className="text-2xl">
+        <strong className="font-bold uppercase font-title">Waiter</strong>
+        <span className="pl-0.5 font-extralight uppercase">App</span>
       </span>
     </p>
   );
